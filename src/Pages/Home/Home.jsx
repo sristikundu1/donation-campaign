2430e10efa809133415edee26af41f7b0774408a
sristikundu1 <nidhukundu1234@gmail.com>
2023-09-27 02:19:20 +0600

    show 12 donation card  of 4 category in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
+import { useLoaderData } from "react-router-dom";
+import Donations from "../../Components/Donations/Donations";
 import Banner from "../../Components/Header/Banner/Banner";
 
 
 const Home = () => {
+    const donations = useLoaderData();
     return (
         <div>
             <Banner></Banner>
+            <Donations donations ={donations}></Donations>
         </div>
     );
 };
